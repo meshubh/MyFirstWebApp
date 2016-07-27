396e7afdde84f2d41044c3ae26cc71d3e68615cf
meshubh <shubhamagarwal9625@gmail.com>
2016-07-28 00:14:30 +0530

    My application's deployment

--- AaoKhaoJao/views.py
+++ AaoKhaoJao/views.py
@@ -37,7 +37,7 @@ def user_login(request):
                 # We'll send the user back to the homepage.
                 login(request, user)
                 response = HttpResponse("", status=302)
-                response['Location'] = "//aaokhaojaoapp.herokuapp.com/secondpage.html"
+                response['Location'] = "//aaokhaojaoapp.herokuapp.com/secondpage"
                 return response
             else:
                 # An inactive account was used - no logging in!
